013e87615e8ebfd9814f1d0aed71657a4ed59934
SWillames <sergiowoc@gmail.com>
2016-05-22 14:06:41 -0300

    Implementado os metodos do controller de times.

--- src/java/br/com/controle/TimeBean.java
+++ src/java/br/com/controle/TimeBean.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Map;
 import javax.inject.Named;
 import javax.enterprise.context.Dependent;
+import javax.faces.bean.ManagedBean;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.persistence.EntityManager;
@@ -20,8 +21,7 @@ import javax.servlet.http.HttpServletRequest;
  *
  * @author sergio
  */
-@Named(value = "timeBean")
-@Dependent
+@ManagedBean
 public class TimeBean {
 
     private Time time = new Time();
@@ -40,10 +40,32 @@ public class TimeBean {
     }
     
     public void  prepararAlteracao(){
-        Map<String, String> params = FacesContext.getCurrentInstance().getExternalContext()
+        Map<String, String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+        Long id = Long.parseLong(params.get("id"));
+        EntityManager manager = this.getManager();
+        TimeRepository repository = new TimeRepository(manager);
+        this.time = repository.procura(id);
+    }
+    
+    public void remove(){
+        Map<String, String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+        Long id = Long.parseLong(params.get("id"));
+        EntityManager manager = this.getManager();
+        TimeRepository repository = new TimeRepository(manager);
+        repository.remove(id);
+        this.times = null;
     }
     
     public TimeBean() {
+    }
+    
+    public List<Time> getTimes(){
+        if (this.times == null) {
+            EntityManager manager = this.getManager();
+            TimeRepository repository = new TimeRepository(manager);
+            this.times = repository.getLista();
+        }
+       return this.times;
     }
     
     private EntityManager getManager(){
@@ -51,7 +73,6 @@ public class TimeBean {
         ExternalContext ec = fc.getExternalContext();
         HttpServletRequest request = (HttpServletRequest) ec.getRequest();
         return (EntityManager) request.getAttribute("EntityManager");
-        
     }
     
 }
